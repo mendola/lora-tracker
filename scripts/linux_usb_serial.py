61f89a4e1b6dec3ff14abb0382555e1b8d4563d5
mendola <mendola.a@husky.neu.edu>
2019-04-07 12:36:52 -0400

    Made Demo map displayer work. Just needs an 'api_key.txt' file.

--- scripts/linux_usb_serial.py
+++ scripts/linux_usb_serial.py
@@ -1,11 +1,20 @@
 from serial import Serial
 from serial.serialutil import SerialException
-import pdb
-
 import sys
 import select
 import struct
-from maps_integration import plot_on_map
+from maps_integration import fetch_map
+import matplotlib.pyplot as plt 
+from matplotlib import animation
+import PIL.Image
+import signal
+import threading
+
+current_map = None
+current_coordinates = (0,0)
+global map_needs_updating
+map_needs_updating = False
+STOP = False
 
 mini_gprmc_msg = None
 latitude = b''
@@ -72,21 +81,67 @@ def handle_minigprmc_msg(msg):
     else:
         return fields[0]
 
-with Serial("/dev/ttyACM0", 115200) as ser:
-    ser.flushInput()
-    while True:
-        if ser.inWaiting() > 0: # if data in serial buffer
-            newdata = ''
-            while ser.inWaiting() > 0: # while there's still data in serial buffer
-                raw_in = ser.read(1)  # read one binary character
-               # pdb.set_trace()
-                ret = handle_raw_char(raw_in)
-                if ret is not None:
-                    latitude_degrees = ret[0]
-                    longitude_degrees = ret[1]
-                    print("Node Coordinates are: %f, %f" % (latitude_degrees, longitude_degrees))
-                    plot_on_map(latitude_degrees, longitude_degrees)
+def usb_interface():
+    global current_coordinates
+    global map_needs_updating
+    try:
+        with Serial("/dev/ttyACM0", 115200) as ser:
+            ser.flushInput()
+            while not STOP:
+                if ser.inWaiting() > 0: # if data in serial buffer
+                    newdata = ''
+                    while ser.inWaiting() > 0: # while there's still data in serial buffer
+                        raw_in = ser.read(1)  # read one binary character
+                        ret = handle_raw_char(raw_in)
+                        if ret is not None:
+                            latitude_degrees = ret[0]
+                            longitude_degrees = ret[1]
+                            print("Node Coordinates are: %f, %f" % (latitude_degrees, longitude_degrees))
+                            current_coordinates = (latitude_degrees, longitude_degrees)
+                            map_needs_updating = True
+                line_in = nonblocking_read()  # nonblocking read from keyboard 
+                if line_in is not None:
+                    ser.write(line_in.encode())  # Send keyboard input over serial
+    except Exception:
+        print("Stopping USB thread.")
 
-        line_in = nonblocking_read()  # nonblocking read from keyboard 
-        if line_in is not None:
-            ser.write(line_in.encode())  # Send keyboard input over serial
+def map_loop():
+    global current_map
+    global current_coordinates
+    global map_needs_updating
+    try:
+        current_map = PIL.Image.open('current_map.png')
+    except IOError:
+        fetch_map(40, -40)
+        current_map = PIL.Image.open('current_map.png')
+
+    print("Starting map loop!!!")
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    im = ax.imshow(current_map, animated=True)
+    def update_map(i):
+        global map_needs_updating
+        if map_needs_updating:
+            fetch_map(current_coordinates[0], current_coordinates[1])
+            map_needs_updating = False
+            current_map = PIL.Image.open('current_map.png')
+            im.set_array(current_map)
+    ani = animation.FuncAnimation(fig, update_map, interval=1)
+    plt.show()
+
+
+serial_thread = threading.Thread(target=usb_interface)
+serial_thread.daemon = False
+serial_thread.start()
+
+plotting_thread = threading.Thread(target=map_loop)
+plotting_thread.daemon = False
+plotting_thread.start()
+
+def stop(a, b):
+    global STOP
+    STOP = True
+    raise Exception
+
+signal.signal(signal.SIGINT, stop)
+signal.signal(signal.SIGTERM, stop)
